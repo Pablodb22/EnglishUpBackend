3e222e985b6e3a7b46e7e09f07184db026d68792
Pablo Durán <pabloduranballesteros@gmail.com>
2025-12-01 18:39:22 +0100

    #4 Encriptado las contraseñas del registro

--- englishup-backend/src/Usuario/registro/registro.service.ts
+++ englishup-backend/src/Usuario/registro/registro.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateRegistroDto } from './dto/create-registro.dto';
 import { createClient } from '@supabase/supabase-js'
 import * as dotenv from 'dotenv';
+import * as bcrypt from 'bcryptjs';
+
 dotenv.config();
 
 @Injectable()
@@ -14,11 +16,13 @@ export class RegistroService {
 
   create(createRegistroDto: CreateRegistroDto) {
     
+    const contrasenaHash = bcrypt.hashSync(createRegistroDto.contrasena, 10);
+
     return this.supabaseServer.from('usuarios').insert([
       {
         nombre_completo: createRegistroDto.nombre,
         correo: createRegistroDto.correo,
-        password_hash: createRegistroDto.contrasena,
+        password_hash: contrasenaHash,
         fecha_creacion: createRegistroDto.fecha_creacion
       }
     ]).then(({ error, data }) => {
